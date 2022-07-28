978dd16cbc886aebd4ae5950c14591557baf8e8c
hamiguazzz <429556240@qq.com>
2022-07-28 23:09:19 +0800

    Rebuild and fix bugs
    Update reduce method.
    Use massive indices as convention.

--- Massive/Codes/ThreePoints.m
+++ Massive/Codes/ThreePoints.m
@@ -4,11 +4,28 @@ LogPri["ThreePoints loaded"];
 (* we use the opposite one here, m omitted *)
 
 (*$$Require$$ ReplaceBraNumber, MassOption, ab, sb*)
-bL = ab;
-bLT = sb;
-Reduce3Pt[amp_] := Module[{rule = ruleP1[3]},
-  Return[amp /. rule];
+bL = sb;
+bLT = ab;
+Reduce3Pt[amp_] := Module[{
+  r1, r2,
+  mrules = {h : (bL | bLT)[i_, j_] /; (j + i === 7) -> 1, Power[h : (bL | bLT)[i_, j_] /; (j + i === 7), _] -> 1},
+  rules = ruleP1[3]
+      ~ Join ~ {
+    ab[1, 2] sb[2, 3] -> 0, ab[1, 2] sb[2, 3]^m$_ -> 0, ab[1, 2]^n$_ sb[2, 3] -> 0, ab[1, 2]^n$_ sb[2, 3]^m$_ -> 0,
+    ab[1, 3] sb[2, 3] -> 0, ab[1, 3] sb[2, 3]^m$_ -> 0, ab[1, 3]^n$_ sb[2, 3] -> 0, ab[1, 3]^n$_ sb[2, 3]^m$_ -> 0,
+    ab[2, 3] sb[1, 2] -> 0, ab[2, 3] sb[1, 2]^m$_ -> 0, ab[2, 3]^n$_ sb[1, 2] -> 0, ab[2, 3]^n$_ sb[1, 2]^m$_ -> 0,
+    ab[2, 3] sb[1, 3] -> 0, ab[2, 3] sb[1, 3]^m$_ -> 0, ab[2, 3]^n$_ sb[1, 3] -> 0, ab[2, 3]^n$_ sb[1, 3]^m$_ -> 0}
+      ~ Join ~ ruleSchS ~ Join ~ ruleSchA
+},
+  rules = rules ~ Join ~ mrules;
+  r1 = amp;
+  r2 = amp /. rules // Expand;
+  While[r1 =!= r2, r1 = r2; r2 = r1 /. rules // Expand;];
+  Return@r1;
 ];
+
+Reduce3Pt[amp_List] := (Last@FactorizeBracket@#&) /@ Flatten[Sum2List /@ Reduce3Pt /@ amp] // DeleteDuplicates // DeleteCases[0] ;
+
 Options@Construct3PBasis = {mass -> All};
 Construct3PBasis[spins : {s1_, s2_, s3_}, OptionsPattern[]] :=
     Module[{massOp = MassOption[OptionValue@mass, 3], zeroPos, nonZeroPos, sortedS, f, rule, re},
@@ -20,8 +37,7 @@ Construct3PBasis[spins : {s1_, s2_, s3_}, OptionsPattern[]] :=
       rule = Table[i -> zeroPos[[i]], {i, Length@zeroPos}] ~ Join ~ Table[j + Length@zeroPos -> nonZeroPos[[j]], {j, Length@nonZeroPos}];
       re = f[sortedS];
       re = If[Head@sortedS === List, ReplaceBraNumber[rule] /@ re, {}];
-      re = Reduce3Pt /@ re // DeleteCases[0];
-      Return[re];
+      Return[Reduce3Pt@re];
     ];
 
 Gen2H1L[spins : {h1_, h2_, s_}] /;
@@ -36,12 +52,11 @@ Gen2H1L[spins : {h1_, h2_, s_}] /;
       (* Use less *)
       (*  mn = -(2s + h1 + h2 - 1);*)
       v2 = s + h1 + h2;
-      p1 = bL[1, 3]^v1[[1]] * bL[2, 3]^v1[[2]];
+      p1 = bL[1, 4]^v1[[1]] * bL[2, 4]^v1[[2]];
       p2 = bLT[1, 2]^v2;
       amp = p1 * p2;
       (*    If[exchangeFlag, amp = ReplaceBraNumber[{1->2,2->1}][amp];];*)
-      amp = Reduce3Pt /@ amp // DeleteCases[0];
-      Return@{amp};
+      Return[{amp}];
     ];
 
 Gen1H2LUE[spins : {h_, s1_, s2_}] /;
@@ -54,34 +69,30 @@ Gen1H2LUE[spins : {h_, s1_, s2_}] /;
       nU = s1 + s2 + h;
       nV = s1 + s2 - h;
       If[!(IntegerQ@nU && IntegerQ@nV), Return[{}]];
-      (*4 as temp, 5 as massive 2*)
-      u = bL[1, 4];
-      v = bL[2, 4] * bLT[1, 2];
+      (*10 as temp massive particle,4 as massive 3, 5 as massive 2*)
+      u = bL[1, 10];
+      v = bL[2, 10] * bLT[1, 2];
       (*contract v mostly with 5*)
       amp = If[
         (*group s1 can hold all v*)
         2 * s1 >= nV,
         (*contract all v with 2 and move v to another group by i*)
         Table[
-          ReplaceBraNumber[{4 -> 5}][v]^(nV - i)
-              * ReplaceBraNumber[{4 -> 5}][u]^(2 * s1 - (nV - i))
-              * ReplaceBraNumber[{4 -> 3}][v]^i
-              * ReplaceBraNumber[{4 -> 3}][u]^(nU - (2 * s1 - (nV - i))),
+          ReplaceBraNumber[{10 -> 5}][v]^(nV - i)
+              * ReplaceBraNumber[{10 -> 5}][u]^(2 * s1 - (nV - i))
+              * ReplaceBraNumber[{10 -> 4}][v]^i
+              * ReplaceBraNumber[{10 -> 4}][u]^(nU - (2 * s1 - (nV - i))),
           {i, 0, C - 1}],
         (*contract all u with 3 while switch u to another group by i*)
         Table[
-          ReplaceBraNumber[{4 -> 5}][v]^(2 * s1 - i)
-              * ReplaceBraNumber[{4 -> 5}][u]^i
-              * ReplaceBraNumber[{4 -> 3}][v]^(nV - (2 * s1 - i))
-              * ReplaceBraNumber[{4 -> 3}][u]^(nU - i)  ,
+          ReplaceBraNumber[{10 -> 5}][v]^(2 * s1 - i)
+              * ReplaceBraNumber[{10 -> 5}][u]^i
+              * ReplaceBraNumber[{10 -> 4}][v]^(nV - (2 * s1 - i))
+              * ReplaceBraNumber[{10 -> 4}][u]^(nU - i)  ,
           {i, 0, C - 1}]
       ];
-      (*contract [25]->m and omit m*)
-      amp = amp /. bL[2, 5] -> 1;
-      (*recovery*)
-      (*      amp = ReplaceBraNumber[{5 -> 2}] /@ amp ;*)
-      amp = ReplaceBraNumber[{5 -> 2}] /@ amp // DeleteCases[___ * Power[_, i_] /; i < 0];
-      amp = Reduce3Pt /@ amp // DeleteCases[0];
+      (*Remove terms with denominator*)
+      amp = amp // DeleteCases[___ * Power[_, i_] /; i < 0];
       Return[amp];
     ];
 
@@ -89,25 +100,27 @@ Gen3L[spins : {s1_, s2_, s3_}] /;
     And @@ (IntegerQ /@ (2 * spins)) &&
         And @@ (# >= 0& /@ spins) &&
         IntegerQ@(Plus @@ spins) :=
-    Module[{p1, p2, mrules, frules, ReduceAmp,
+    Module[{p1, p2, mrules, frules, ReduceAmpMass,
       Gen1, Gen2, ps, ps2, amps1, amps2, re},
-      (*4,5 as Temp; 6,7 as Temp; 11,12,13 as massive 1,2,3*)
-      p1 = bL[1, 4] * bL[2, 5] * bLT[1, 2];
-      p2 = bL[6, 7];
+      (*10,11 as Temp; 12,13 as Temp; 6,5,4 as massive 1,2,3*)
+      p1 = bL[1, 10] * bL[2, 11] * bLT[1, 2];
+      p2 = bL[12, 13];
 
       (*reduce*)
-      mrules = {bL[1, 11] -> 1, bL[2, 12] -> 1, bL[3, 13] -> 1};
-      frules = {11 -> 1, 12 -> 2, 13 -> 3};
-      ReduceAmp[amp_] := ReplaceBraNumber[frules][amp /. mrules];
+      mrules = {h : (bL | bLT)[i_, j_] /; (j + i === 7) -> 1, Power[h : (bL | bLT)[i_, j_] /; (j + i === 7), _] -> 1};
+      (*      frules = {11 -> 1, 12 -> 2, 13 -> 3};*)
+      (*      ReduceAmp[amp_] := ReplaceBraNumber[frules][amp /. mrules];*)
+      ReduceAmpMass[amp_] := amp /. mrules;
 
       (* Gen amp from filling lists *)
-      Gen1[{l4_List, l5_List}] := Times @@ Table[
-        ReplaceBraNumber[{4 -> l4[[i]], 5 -> l5[[i]]}]@p1
-        , {i, Length@l4}] // ReduceAmp;
-      Gen2[{l4_List, l5_List, {x6_, x7_}}] := Gen1[{l4, l5}] * (ReplaceBraNumber[{6 -> x6, 7 -> x7}][p2] // ReduceAmp);
+      Gen1[{la_List, lb_List}] /; (Length@la === Length@lb) := Times @@ Table[
+        ReplaceBraNumber[{10 -> la[[i]], 11 -> lb[[i]]}]@p1
+        , {i, Length@la}] // ReduceAmpMass;
+      Gen2[{la_List, lb_List, {xa_, xb_}}] /; (Length@la === Length@lb) :=
+          Gen1[{la, lb}] * (ReplaceBraNumber[{12 -> xa, 13 -> xb}][p2] // ReduceAmpMass);
 
       (*All indices list*)
-      ps = Permutations[Join[Table[11, 2 * s1], Table[12, 2 * s2], Table[13, 2 * s3]]];
+      ps = Permutations[Join[Table[6, 2 * s1], Table[5, 2 * s2], Table[4, 2 * s3]]];
 
       (*Type I, i = 0*)
       ps2 = {Sort[#[[1 ;; s1 + s2 + s3]]], Sort[#[[s1 + s2 + s3 + 1 ;;]]]}& /@ ps // DeleteDuplicates;
@@ -117,14 +130,13 @@ Gen3L[spins : {s1_, s2_, s3_}] /;
       ps2 = {Sort[#[[1 ;; s1 + s2 + s3 - 1]]], Sort[#[[s1 + s2 + s3 ;; -3]]], Sort[#[[-2 ;; -1]]]}& /@ ps //
           DeleteDuplicates // DeleteCases[{_, _, {i_, j_}} /; i == j];
       amps2 = Gen2 /@ ps2;
-      re = Reduce3Pt /@ re // DeleteCases[0];
       re = (Last@FactorizeBracket@#&) /@ Join[amps1, amps2] // DeleteDuplicates;
       Return[re];
     ];
 
 (* Check method ref:Dong*)
-
-Check3PointCheckAmount[spins : {s1_, s2_, s3_}, OptionsPattern[]] :=
+Options@Check3PointAmount = {mass -> All};
+Check3PointAmount[spins : {s1_, s2_, s3_}, OptionsPattern[]] :=
     Module[{massOp = MassOption[OptionValue@mass, 3], zeroPos, nonZeroPos, sortedS, f, rule, re},
       zeroPos = If[KeyExistsQ[#, 0], #[0], {}] &[PositionIndex@massOp];
       nonZeroPos = Complement[Range[3], zeroPos];
@@ -135,7 +147,7 @@ Check3PointCheckAmount[spins : {s1_, s2_, s3_}, OptionsPattern[]] :=
       Return@re;
     ];
 
-CheckAmount2H1L[spins : {h1_, h2_, s_}] := With[
+CheckAmount2H1L[spins : {h1_, h2_, s_}] := With[{},
   If[!(And @@ (IntegerQ /@ (2 * spins)) &&
       s >= 0 &&
       IntegerQ@(Plus @@ spins)), Return[0]];
@@ -144,7 +156,7 @@ CheckAmount2H1L[spins : {h1_, h2_, s_}] := With[
     Return[1]];
 ];
 
-CheckAmount1H2LUE[spins : {h_, s1_, s2_}] := With[
+CheckAmount1H2LUE[spins : {h_, s1_, s2_}] := With[{},
   If[!(And @@ (IntegerQ /@ (2 * spins)) &&
       s1 >= 0 && s2 >= 0 &&
       IntegerQ@(Plus @@ spins)), Return[0]];
@@ -154,13 +166,13 @@ CheckAmount1H2LUE[spins : {h_, s1_, s2_}] := With[
 ];
 
 CheckAmount3L[spins : {s1_, s2_, s3_}] := Module[
-  {p},
+  {s, p},
   If[!(And @@ (IntegerQ /@ (2 * spins)) &&
       And @@ (# >= 0& /@ spins) &&
       IntegerQ@(Plus @@ spins)), Return@0];
-  spins = Sort@spins;
-  p = Max[s1 + s2 - s3, 0];
-  Return[(2 * s1 + 1) * (2 * s2 + 1) - p * (p + 1)];
+  s = Sort@spins;
+  p = Max[s[[1]] + s[[2]] - s[[3]], 0];
+  Return[(2 * s[[1]] + 1) * (2 * s[[2]] + 1) - p * (p + 1)];
 ];
 
 
